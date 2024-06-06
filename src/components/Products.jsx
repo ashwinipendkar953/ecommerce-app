497b172b17dc57663b499fa6135d8bf7c403d438
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2024-06-06 19:22:03 +0530

    refactor: product.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,7 +13,10 @@ const Products = () => {
       <p>
         <span className="fw-bold fs-5">Showing All Products</span>{" "}
         <span className="px-3">
-          ( Showing {data.length} {data.length === 1 ? "Product" : "products"} )
+          ( Showing{" "}
+          {data &&
+            `${data.length} ${data.length === 1 ? "product" : "products"}`}{" "}
+          )
         </span>
       </p>
       <div className="row g-5 pt-2 pb-5">
@@ -27,35 +30,29 @@ const Products = () => {
 
         {data &&
           data.map((product) => {
-            const discountedPrice =
-              product.price -
-              product.price * (product.discountPercentage / 100);
+            const discountedPrice = parseInt(
+              product.price - product.price * (product.discountPercentage / 100)
+            );
             return (
               <div className="col-md-4" key={product._id}>
                 <div className="card">
                   <img
-                    src={product.productImage}
-                    alt={product.productName}
+                    src={product.image}
+                    alt={product.name}
                     className="card-img-top"
+                    style={{ height: "317px" }}
                   />
+
                   <div className="card-body">
                     <div className="d-flex justify-content-between">
                       <div>
-                        <h6 className="card-title ">{product.productName}</h6>
-                        <p className="card-text">
-                          <span className="fw-bold fs-5">
-                            <i className="bi bi-currency-rupee"></i>
-                            {discountedPrice}
-                          </span>
-                          <span className="text-decoration-line-through px-2 text-secondary">
-                            <i className="bi bi-currency-rupee"></i>
-                            {product.price}
-                          </span>
-
-                          <span className="text-success fw-semibold">
-                            <small>{product.discountPercentage}% off</small>
-                          </span>
-                        </p>
+                        <h6 className="card-title text-secondary">
+                          {product.brand}
+                        </h6>
+                        <h6 className="fw-normal">{`${product.name.slice(
+                          0,
+                          25
+                        )}...`}</h6>
                       </div>
                       <Link className="text-dark btn-outline-dark">
                         <i
@@ -63,6 +60,23 @@ const Products = () => {
                           style={{ fontSize: "25px" }}
                         ></i>
                       </Link>
+                    </div>
+
+                    <div>
+                      <p className="card-text">
+                        <span className="fw-bold fs-5">
+                          <i className="bi bi-currency-rupee"></i>
+                          {discountedPrice}
+                        </span>
+                        <span className="text-decoration-line-through px-2 text-secondary">
+                          <i className="bi bi-currency-rupee"></i>
+                          {product.price}
+                        </span>
+
+                        <span className="text-success fw-semibold">
+                          <small>{product.discountPercentage}% off</small>
+                        </span>
+                      </p>
                     </div>
 
                     <div className="d-grid mt-2 mx-auto">
@@ -75,6 +89,51 @@ const Products = () => {
               </div>
             );
           })}
+
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/8/u/h/s-popcorn-black-praizy-original-imahydwe7rzkb8mk.jpeg?q=70"
+              alt="product image"
+              className="card-img-top"
+              style={{ height: "317px" }}
+            />
+            <div className="card-body">
+              <div className="d-flex justify-content-between">
+                <div>
+                  <h6 className="card-title text-secondary">Brand Name</h6>
+                  <h6 className="fw-normal">Product Name</h6>
+                </div>
+                <Link className="text-dark btn-outline-dark">
+                  <i className="bi bi-heart" style={{ fontSize: "25px" }}></i>
+                </Link>
+              </div>
+
+              <div>
+                <p className="card-text">
+                  <span className="fw-bold fs-5">
+                    <i className="bi bi-currency-rupee"></i>
+                    1500
+                  </span>
+                  <span className="text-decoration-line-through px-2 text-secondary">
+                    <i className="bi bi-currency-rupee"></i>
+                    3000
+                  </span>
+
+                  <span className="text-success fw-semibold">
+                    <small>50% off</small>
+                  </span>
+                </p>
+              </div>
+
+              <div className="d-grid mt-2 mx-auto">
+                <Link className="btn btn-primary rounded-0" type="button">
+                  Go to Cart
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
