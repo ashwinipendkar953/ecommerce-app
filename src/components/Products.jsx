b0749cd9344b1d36b64655d372996e71ef8a4ada
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2024-06-14 20:42:59 +0530

    refactor: products.js

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -26,13 +26,14 @@ const Products = ({ data, loading, error }) => {
             const discountedPrice = parseInt(
               product.price - product.price * (product.discountPercentage / 100)
             );
+
             return (
-              <div className="col-md-6 col-lg-4 col-xl-3" key={product._id}>
-                <div className="card rounded-0 h-100">
+              <div className="col-sm-6 col-lg-4" key={product._id}>
+                <div className="card h-100 position-relative">
                   <img
                     src={product.image}
                     alt={product.name}
-                    className="card-img-top rounded-0"
+                    className="card-img-top"
                     style={{
                       width: "100%",
                       height: "300px",
@@ -46,53 +47,62 @@ const Products = ({ data, loading, error }) => {
                         <h6 className="card-title text-muted">
                           {product.brand}
                         </h6>
-                        <h6 className="fw-normal">
-                          <small className="d-none d-lg-inline">{`${product.name.slice(
-                            0,
-                            18
-                          )}...`}</small>
-                          <small className="d-none d-md-inline d-lg-none">
-                            {product.name.slice(0, 25)}...
-                          </small>
-                          <small className="d-inline d-md-none">
-                            {product.name.slice(0, 35)}...
-                          </small>
+                        <h6 className="fw-semibold">
+                          {product.name.slice(0, 30)}...
                         </h6>
                       </div>
-                      <Link
-                        className={`btn-outline-dark ${
-                          product.isWishlisted ? "text-pink" : "text-dark"
-                        }`}
-                      >
-                        <i
-                          className="bi bi-heart-fill"
-                          style={{ fontSize: "25px" }}
-                        ></i>
-                      </Link>
                     </div>
 
+                    {/* price */}
                     <div>
                       <p className="card-text">
-                        <span className="fw-bold fs-5">
-                          <i className="bi bi-currency-rupee"></i>
-                          {discountedPrice}
+                        <span className="text-secondary">MRP: </span>
+                        <span className="text-decoration-line-through text-secondary mx-1 ">
+                          ₹{product.price}
                         </span>
-                        <span className="text-decoration-line-through px-2 text-secondary">
-                          <i className="bi bi-currency-rupee"></i>
-                          {product.price}
-                        </span>
-
-                        <span className="text-pink fw-semibold">
-                          <small>{product.discountPercentage}% off</small>
+                        <span className="fw-semibold ">₹{discountedPrice}</span>
+                        <span className="mx-1">|</span>
+                        <span className="text-success">
+                          {product.discountPercentage}% off
                         </span>
                       </p>
                     </div>
+
+                    {/* rating */}
+                    <div>{product.rating}</div>
                   </div>
 
-                  <div className="card-footer btn-pink text-center rounded-0 ">
-                    <Link className="text-light text-decoration-none fw-semibold">
-                      Add to Cart
-                    </Link>
+                  <div
+                    className="row "
+                    style={{
+                      marginLeft: "0.4px",
+                      marginRight: "0.4px",
+                      height: "45px",
+                    }}
+                  >
+                    <div className="col-3 center-content border-top">
+                      <Link type="button" className="text-pink">
+                        {product.isWishlisted ? (
+                          <i
+                            className="bi bi-heart-fill"
+                            style={{ fontSize: "25px" }}
+                          ></i>
+                        ) : (
+                          <i
+                            className="bi bi-heart"
+                            style={{ fontSize: "25px" }}
+                          ></i>
+                        )}
+                      </Link>
+                    </div>
+                    <div
+                      className="col-9 center-content bg-pink"
+                      style={{ borderRadius: "0 0 5px 0" }}
+                    >
+                      <Link className="text-light text-decoration-none fw-semibold">
+                        Add to Cart
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
